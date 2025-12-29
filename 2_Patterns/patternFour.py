0343b384a053e692c0c851dda5fce8c13be5885a
Shah Nisha (MS/ESW2-TM-XC) <nisha.shah@in.bosch.com>
2025-12-30 00:23:32 +0530

    pattern problem

--- 2_Patterns/patternFour.py
+++ 2_Patterns/patternFour.py
@@ -1,11 +1,11 @@
 class Pattern:
-    def patternThree(self, n):
+    def patternFour(self, n):
         for i in range(n):
             for j in range(i+1):
-                print(j+1, end=" ")
+                print(i+1, end=" ")
             print()
 
 if __name__=="__main__":
     obj=Pattern()
     num=int(input("Enter the value of n: "))
-    obj.patternThree(num)
+    obj.patternFour(num)

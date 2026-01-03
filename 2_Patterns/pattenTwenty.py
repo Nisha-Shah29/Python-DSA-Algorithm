64c10a3e4940e54eec131600aa9e840d461b508c
Shah Nisha (MS/ESW2-TM-XC) <nisha.shah@in.bosch.com>
2026-01-03 19:52:22 +0530

    pattern problem20

---
+++ 2_Patterns/pattenTwenty.py
@@ -0,0 +1,29 @@
+class Pattern:
+    def patternUpward(self, n):
+        for i in range(n):
+            for j in range(n-i):
+                print("*", end=" ")
+            for k in range(2*i):
+                print(" ", end=" ")
+            for l in range(n-i):
+                print("*", end=" ")
+            print()
+
+    def patternDownward(self, n):
+        for i in range(n):
+            for j in range(i+1):
+                print("*", end=" ")
+            for k in range((2*n)-(2*(i+1))):
+                print(" ", end=" ")
+            for l in range(i+1):
+                print("*", end=" ")
+            print()
+
+    def patternTwenty(self, n):
+        self.patternUpward(n)
+        self.patternDownward(n)
+
+if __name__=="__main__":
+    obj=Pattern()
+    num=int(input("Enter the value of n: "))
+    obj.patternTwenty(num)

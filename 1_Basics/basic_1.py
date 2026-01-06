d8a4e96efd12776aafd35b0ed61ac0c207f9392d
Shah Nisha (MS/ESW2-TM-XC) <nisha.shah@in.bosch.com>
2026-01-06 18:33:17 +0530

    basic problem 1

---
+++ 1_Basics/basic_1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def readAndPrintNum(self):
+        a=int(input("Enter a number: "))
+        print(a)
+
+if __name__=="__main__":
+    sol=Solution()
+    sol.readAndPrintNum()

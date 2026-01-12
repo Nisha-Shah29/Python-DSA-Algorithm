728f0365d85d03d8006269dd2e5221a881f34835
Shah Nisha (MS/ESW2-TM-XC) <nisha.shah@in.bosch.com>
2026-01-12 16:05:36 +0530

    basic problem 4

---
+++ 1_Basics/basic_4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def weekDay(self, day):
+        if day==1:
+            print("monday")
+        elif day==2:
+            print("tuesday")
+        elif day==3:
+            print("wednesday")
+        elif day==4:
+            print("thursday")
+        elif day==5:
+            print("friday")
+        elif day==6:
+            print("saturday")
+        elif day==7:
+            print("sunday")
+        else:
+            print("Invalid")
+
+if __name__=="__main__":
+    sol=Solution()
+    num=int(input("Enter the day number: "))
+    sol.weekDay(num)
